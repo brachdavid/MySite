a76cd1a1a6984becc3a8f29d77ea63600b3f0a20
brachdavid <153853659+brachdavid@users.noreply.github.com>
2025-01-05 16:20:01 +0100

    Add a WebsitesList component. Add a model ProjectType.
    - Create a new WebsitesList.razor component with proposals of websites, which i can make. Add dynamic data to websites list.
    - Add a new Models folder to project.
    - Create a new model ProjectType.cs, which is connected with WebsitesList.razor component.

---
+++ Components/Models/ProjectType.cs
@@ -0,0 +1,22 @@
+﻿namespace MySite.Components.Models
+{
+    /// <summary>
+    /// Třída reprezentující druh webové aplikace
+    /// </summary>
+    public class ProjectType
+    {
+        /// <summary>
+        /// Název webové aplikace
+        /// </summary>
+        public required string Title { get; set; }
+        /// <summary>
+        /// Krátký popis webové aplikace
+        /// </summary>
+        public required string ShortDescription { get; set; }
+        /// <summary>
+        /// Detailní popis webové aplikace
+        /// </summary>
+        public required string DetailedDescription { get; set; }
+        public required string IconClass { get; set; }
+    }
+}
