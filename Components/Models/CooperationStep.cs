f9eac8d4430176c87cc091589bee050ceb2e5415
brachdavid <153853659+brachdavid@users.noreply.github.com>
2025-01-30 22:02:50 +0100

    Complete contact section with form, direct contacts, and Calendly integration
    - Implement `Contact.razor` as the main contact section with `ContactForm`, `OtherContacts`, and `Calendly` components.
    - Develope `ContactForm.razor` with input validation, form submission logic, and email sending functionality.
    - Create `OtherContacts.razor` displaying various direct contact methods (email, phone, WhatsApp, Facebook, LinkedIn).
    - Add `Calendly.razor` component for scheduling online consultations via Calendly.
    - Apply responsive Bootstrap styling for a clean and accessible layout across devices.

--- Components/Models/CooperationStep.cs
+++ Components/Models/CooperationStep.cs
@@ -2,8 +2,17 @@
 {
     public class CooperationStep
     {
+        /// <summary>
+        /// Číslo kroku
+        /// </summary>
         public required int Order { get; set; }
+        /// <summary>
+        /// Název kroku
+        /// </summary>
         public required string StepName { get; set; }
+        /// <summary>
+        /// Popis kroku
+        /// </summary>
         public required string StepDescription { get; set; }
     }
 }
---
+++ Components/Models/EmailRequest.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySite.Components.Models
+{
+    /// <summary>
+    /// Typ dotazu
+    /// </summary>
+    public enum QueryType
+    {
+        [Display(Name = "Tvorba webovek")]
+        WebCreate = 1,
+        [Display(Name = "Úprava webovek")]
+        WebUpdate = 2,
+        [Display(Name = "Správa webovek")]
+        WebAdministation = 3,
+        [Display(Name = "Tvůrčí psaní")]
+        CreativeWriting = 4,
+        [Display(Name = "Nabídka práce")]
+        JobOffer = 5
+    }
+    public class EmailRequest
+    {
+        /// <summary>
+        /// Jméno toho, kdo pokládá dotaz
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// E-mail toho, kdo pokládá dotaz
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Telefonní číslo toho, kdo pokládá dotaz
+        /// </summary>
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Typ dotazu
+        /// </summary>
+        public QueryType QueryType { get; set; } = QueryType.WebCreate;
+
+        /// <summary>
+        /// Zpráva pro příjemce dotazu
+        /// </summary>
+        public string? Message { get; set; }
+    }
+}
